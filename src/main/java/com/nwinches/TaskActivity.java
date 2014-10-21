e76dc1948d035dc309e91f286b3bd2e5e3d2ad1c
Winchester <nwinches@00264a12603e.ant.amazon.com>
2014-10-20 20:09:46 -0700

    exceptions and powermock

--- src/main/java/com/nwinches/TaskActivity.java
+++ src/main/java/com/nwinches/TaskActivity.java
@@ -4,16 +4,23 @@ import java.util.List;
 import java.util.UUID;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
 import com.nwinches.dao.InMemoryTaskDao;
 import com.nwinches.dao.TaskDao;
 import com.nwinches.entity.Task;
+import com.nwinches.exception.NoSuchTaskException;
 
+/**
+ * Basic class for handling RESTful HTTP requests regarding tasks.
+ */
 @Path("task")
 public class TaskActivity {
   
@@ -34,7 +41,12 @@ public class TaskActivity {
   //@GET
   //@Produces(MediaType.APPLICATION_JSON)
   public Task getTask(String taskId) {
-    return taskDao.getTask(taskId);
+    try {
+      return taskDao.getTask(taskId);
+    } catch (NoSuchTaskException e) {
+      Response.status(Status.NOT_FOUND).build().close();
+      return null;
+    }
   }
   
   @POST
@@ -43,5 +55,11 @@ public class TaskActivity {
     task.setId(UUID.randomUUID().toString());
 
     taskDao.saveTask(task);
+  }
+  
+  @DELETE
+  @Consumes(MediaType.TEXT_PLAIN)
+  public void deleteTask(String taskId) {
+    taskDao.deleteTask(taskId);
   }
 }
